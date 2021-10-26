1d4a2e2f5c65f22aedcdd7e41574d311bd3f1b88
jevinjivani2507 <jivani.jevin25@gmail.com>
2021-10-26 23:33:03 +0530

    Some of Changes

--- Stacks&Queues/Implemantion/MyQueue.java
+++ Stacks&Queues/Implemantion/MyQueue.java
@@ -1,11 +1,7 @@
-
-// "static void main" must be defined in a public class.
 import java.util.*;
 
 class Queue {
-    // store elements
     private List<Integer> data;
-    // a pointer to indicate the start position
     private int p_start;
 
     public Queue() {
@@ -13,17 +9,11 @@ class Queue {
         p_start = 0;
     }
 
-    /**
-     * Insert an element into the queue. Return true if the operation is successful.
-     */
     public boolean enQueue(int x) {
         data.add(x);
         return true;
     };
 
-    /**
-     * Delete an element from the queue. Return true if the operation is successful.
-     */
     public boolean deQueue() {
         if (isEmpty() == true) {
             return false;
@@ -32,12 +22,10 @@ class Queue {
         return true;
     }
 
-    /** Get the front item from the queue. */
     public int Front() {
         return data.get(p_start);
     }
-
-    /** Checks whether the queue is empty or not. */
+    
     public boolean isEmpty() {
         return p_start >= data.size();
     }
